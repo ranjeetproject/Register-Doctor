c6b787c3f6f195b3425b94bb174957ed43c197d6
Swapan Kundu <swapankm.brainium@gmail.com>
2021-02-20 00:49:40 +0530

    frontend pages

---
+++ resources/views/frontend/about-us.blade.php
@@ -0,0 +1,5 @@
+@extends('frontend.beforeloginlayout.app')
+
+@section('content')
+
+@endsection
--- resources/views/frontend/pharmacist/afterloginlayout/common_navigation.blade.php
+++ resources/views/frontend/pharmacist/afterloginlayout/common_navigation.blade.php
@@ -4,7 +4,7 @@
       <a href="#" class="for-email">{{Auth::user()->email}}</a>
       <div class="profile-nav-img"><img src="{{ asset('public/images/frontend/images/profile-pic.jpg') }}" alt="" ><span>{{Auth::user()->name}}</span></div>
       <div class="ser-log">
-        <a class="ser-log-list" href="#"><i class="fal fa-search"></i></a>
+        {{-- <a class="ser-log-list" href="#"><i class="fal fa-search"></i></a> --}}
         <a class="ser-log-list" href="{{ route('logout')}}" onclick="event.preventDefault();document.getElementById('logout-form').submit();"><img src="{{ asset('public/images/frontend/images/prof-logout.png') }}" alt=""></a>
         <form id="logout-form" action="{{route('logout')}}" method="POST"style="display: none;">
           @csrf
