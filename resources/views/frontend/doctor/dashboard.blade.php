520b4d01d24c74951b2e00a9ba14f542fa5c9cef
sandippyne <sandip.pyne@brainiuminfotech.com>
2021-05-28 20:26:37 +0530

    Design_changes

--- resources/views/frontend/doctor/dashboard.blade.php
+++ resources/views/frontend/doctor/dashboard.blade.php
@@ -4,22 +4,27 @@
     <div class="col Prescription-dashboard-right">
         <div class="row">
 
-            <div class="col">
-                <h3>INCOMING REQUESTS FOR YOU</h3>
+            <div class="col-sm-12">
+                <h3 class="text-center mb-3">INCOMING REQUESTS</h3>
                 <div class="row incoming-request">
                     
-                    <a href="{{route('doctor.prescriptions')}}" class="card col mr-2">
+                    <a href="{{route('doctor.prescriptions')}}" class="card new-crd col-sm-6">
                         <img src="{{ asset('public/images/frontend/images/ex-icon.png') }}" class="for-tool-tip" data-toggle="tooltip" data-placement="right" title="" data-original-title="One line definition" alt="">
                         <div class="card-cont">
+                        <img src="{{ asset('public/images/frontend/images/Live-Text-Chat.png') }}" alt="">
+                        <img src="{{ asset('public/images/frontend/images/Live-Video-Chat.png') }}" alt="">
+                            <img src="{{ asset('public/images/frontend/images/Booked-Question.png') }}" alt="">
                             <img src="{{ asset('public/images/frontend/images/Prescriptions.png') }}" alt="">
-                            <h4>Prescriptions<br><span>Waiting</span></h4>
+                            <h4>Booking <br><span>Patient Chose You Accept or Decline</span></h4>
                         </div>
                     </a>
 
-                    <a href="{{route('doctor.cases','quick-questions')}}" class="card col">
+                    <a href="{{route('doctor.cases','quick-questions')}}" class="card new-crd col-sm-6">
                         <div class="card-cont">
+                        
+                        <img src="{{ asset('public/images/frontend/images/Prescriptions.png') }}" alt="">
                             <img src="{{ asset('public/images/frontend/images/Quick-Question.png') }}" alt="">
-                            <h4>Quick Question<br><span>Waiting</span></h4>
+                            <h4>Quick Question Prescriptions<br><span>Any Doctor to Take</span></h4>
                         </div>
                     </a>
                  
@@ -28,37 +33,44 @@
 
 
             <div class="col">
-                <h3>YOUR Accepted CASES</h3>
+                <!-- <h3>YOUR Accepted CASES</h3> -->
                 <div class="incoming-request row">
                     {{-- <a href="#" class="card">
                         <img src="{{ asset('public/images/frontend/images/ex-icon.png') }}" class="for-tool-tip" data-toggle="tooltip" data-placement="right" title="" data-original-title="One line definition" alt="">
                     
                         <div class="card-cont">
                             <img src="{{ asset('public/images/frontend/images/Prescriptions.png') }}" alt="">
-                            <h4>Prescriptions<br><span>Waiting</span></h4>
+                            <h4>Accepted Cases</h4>
                         </div>
                     </a> --}}
-                    <a href="{{route('doctor.cases',['live-video','accepted'])}}" class="card col">
+                    <a href="{{route('doctor.cases',['live-video','accepted'])}}" class="card new-crd col-sm-6">
                         <div class="card-cont">
-                            <img src="{{ asset('public/images/frontend/images/Live-Video-Chat.png') }}" alt="">
-                            <h4>Consultations Video, Live Chat, Q/A, QQ <br><span>Upcaming</span></h4>
+                        <img src="{{ asset('public/images/frontend/images/Live-Text-Chat.png') }}" alt="">
+                        <img src="{{ asset('public/images/frontend/images/Live-Video-Chat.png') }}" alt="">
+                            <img src="{{ asset('public/images/frontend/images/Booked-Question.png') }}" alt="">
+                            <img src="{{ asset('public/images/frontend/images/Quick-Question.png') }}" alt="">
+                            <img src="{{ asset('public/images/frontend/images/Prescriptions.png') }}" alt="">
+                            <h4>Accepted Cases</h4>
                         </div>
                     </a>
-                    
+                    <div class="col-sm-6 Queries">
+                <a href="#">
+                    <h2>Queries<br><small><strong>Messages from Patients, Parmacists, Admin</strong></small></h2>
+                    <img src="{{ asset('public/images/frontend/images/Hello-icon.png') }}" alt="">
+                </a>
+            </div>
                 </div>
             </div>
 
 
 </div>
-<div class="row">
+        <!-- <div class="row">
             <div class="col-sm-12 Queries">
                 <a href="#">
                     <h2>Queries<br><small><strong>Messages from Patients, Parmacists, Admin</strong></small></h2>
                     <img src="{{ asset('public/images/frontend/images/Hello-icon.png') }}" alt="">
                 </a>
             </div>
-            
-            
-        </div>  
+        </div>   -->
     </div>
 @endsection
