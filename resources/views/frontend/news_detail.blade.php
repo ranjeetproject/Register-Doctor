0dd09a849c7bef0c306762779e2937adc326e57d
chandan.brainium <chandan.kamila@brainiuminfotech.com>
2021-10-28 18:26:54 +0530

    News and news details pages UI modification

--- resources/views/frontend/news_detail.blade.php
+++ resources/views/frontend/news_detail.blade.php
@@ -1,17 +1,17 @@
 @extends('frontend.beforeloginlayout.app')
 
 @section('content')
-    <section class="for-w-100 main-content innerpage news-page">
+    <section class="for-w-100 main-content innerpage news-page news-details-page">
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
                     <h1 class="inner-page-title">
-                        News
+                        News Details
                     </h1>
                 </div>
             </div>
             <div class="row">
-                <div class="col-sm-12">
+                <div class="col-md-8">
 {{-- @dd($news) --}}
                     <div class="news-listing for-w-100">
                         <div class="news-listing-top for-w-100">
@@ -32,6 +32,14 @@
                         </div>
                     </div>
 
+                </div>
+                <div class="col-md-4">
+                    <div class="resent-post for-w-100">
+                        <h3>Recent Posts</h3>
+                        <div class="resent-listing for-w-100">
+                          
+                        </div>
+                    </div>
                 </div>
 
             </div>
